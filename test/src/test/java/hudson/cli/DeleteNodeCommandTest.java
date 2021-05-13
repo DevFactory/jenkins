bc0bb6f58b39db6d3c16542ac71a9ef482f64649
codefix-service-user <36582944+codefix-service-user@users.noreply.github.com>
2021-05-13 08:04:47 +0200

    Codefix-9999407-[63402726-60]

--- test/src/test/java/hudson/cli/DeleteNodeCommandTest.java
+++ test/src/test/java/hudson/cli/DeleteNodeCommandTest.java
@@ -24,6 +24,8 @@
 
 package hudson.cli;
 
+import hudson.cli.CLICommandInvoker.Result;
+
 import hudson.model.Computer;
 import jenkins.model.Jenkins;
 import org.junit.Before;
@@ -99,9 +101,7 @@ public class DeleteNodeCommandTest {
                 .authorizedTo(Computer.DELETE, Jenkins.READ)
                 .invokeWithArgs("aNode1", "aNode2", "aNode3");
 
-        assertThat(result, succeededSilently());
-        assertThat(j.jenkins.getView("aNode1"), nullValue());
-        assertThat(j.jenkins.getView("aNode2"), nullValue());
+        extractedMethod60505(result);
         assertThat(j.jenkins.getView("aNode3"), nullValue());
     }
 
@@ -192,6 +192,10 @@ public class DeleteNodeCommandTest {
                 .authorizedTo(Computer.DELETE, Jenkins.READ)
                 .invokeWithArgs("aNode1", "aNode2", "aNode1");
 
+        extractedMethod60505(result);
+    }
+
+    private void extractedMethod60505(final CLICommandInvoker.Result result) {
         assertThat(result, succeededSilently());
         assertThat(j.jenkins.getView("aNode1"), nullValue());
         assertThat(j.jenkins.getView("aNode2"), nullValue());
