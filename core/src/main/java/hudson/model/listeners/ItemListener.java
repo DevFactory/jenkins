aae620ef449694d299ce0cf4ffc8d0bbdcad078f
codefix-service-user <36582944+codefix-service-user@users.noreply.github.com>
2021-05-13 07:47:15 +0200

    Codefix-9999407-[63402726-374]

--- core/src/main/java/hudson/model/listeners/ItemListener.java
+++ core/src/main/java/hudson/model/listeners/ItemListener.java
@@ -260,24 +260,24 @@ public class ItemListener implements ExtensionPoint {
                 }
             });
         }
-        forAll(l -> {
-            if (l!= null) {
-                l.onLocationChanged(rootItem, oldFullName, newFullName);
-            }
-        });
+        extractedMethod48812(rootItem, oldFullName, newFullName);
         if (rootItem instanceof ItemGroup) {
             for (final Item child : Items.allItems2(ACL.SYSTEM2, (ItemGroup)rootItem, Item.class)) {
                 final String childNew = child.getFullName();
                 assert childNew.startsWith(newFullName);
                 assert childNew.charAt(newFullName.length()) == '/';
                 final String childOld = oldFullName + childNew.substring(newFullName.length());
-                forAll(l -> {
-                    if (l != null) {
-                        l.onLocationChanged(child, childOld, childNew);
-                    }
-                });
+                extractedMethod48812(child, childOld, childNew);
             }
         }
+    }
+
+    private static void extractedMethod48812(final Item child, final String childOld, final String childNew) {
+        forAll(l -> {
+            if (l != null) {
+                l.onLocationChanged(child, childOld, childNew);
+            }
+        });
     }
 
 }
